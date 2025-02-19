8193216264c54bb158129e6986ef9885901419d2
Gurmeher Singh <gurumehersingh1706@gmail.com>
2025-02-19 09:57:16 -0500

    Unit 5

---
+++ inventory/categories.sql
@@ -0,0 +1,8 @@
+create Table categories (
+    categoryID  INT(11) NOT NULL,
+    categoryCode VARCHAR(10)  NOT NULL,
+    categoryName  VARCHAR(255)  NOT NULL,
+    PRIMARY KEY (categoryID)
+);
+select * from categories;
+select * from categories;
