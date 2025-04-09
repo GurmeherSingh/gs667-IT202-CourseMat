dd9ffbb2a5eb40675ed31825a4f755969a12e1a4
Gurmeher Singh <gurumehersingh1706@gmail.com>
2025-04-09 10:25:01 -0400

    comment

--- inventory/index.php
+++ inventory/index.php
@@ -1,10 +1,25 @@
 <?php
 session_start();
+include("category.php");
+include("config.php");
+include("item.php");
 ?>
 <!DOCTYPE html>
 <html>
-<head><title>Inventory Helper</title></head>
+<head><title>Inventory Helper</title>
+<link rel="stylesheet" type="text/css" href="ih_styles.css">
+<link rel="icon" type="image/png" href="C:\Users\leo18\Downloads\logo.png"></head>
 <body>
+<header>
+       <?php include("header.inc.php"); ?>
+   </header>
+   <section style="height: 425px;">
+       <nav style="float: left; height: 100%;">
+           <?php include("nav.inc.php"); ?>
+       </nav>
+         <section style="height: 375px;">
+      <nav style="float: left; height: 100%; min-width: 175px; width: auto;">
+          <?php include("nav.inc.php"); ?>
    <section id="container">
        <main>
            <?php
@@ -16,4 +31,7 @@ session_start();
            ?>
        </main>
    </section>
+   <footer>
+       <?php include("footer.inc.php"); ?>
+   </footer>
 </body>
